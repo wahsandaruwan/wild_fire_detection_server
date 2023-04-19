fbb42075ca2190740f4afeedee4c7e37738f87cd
wahsandaruwan <wahsandaruwan6@gmail.com>
2023-04-19 21:02:42 +0530

    Added model using function

---
+++ UseModel.py
@@ -0,0 +1,21 @@
+# -----Imports-----
+import pickle as pk
+import numpy as np
+
+# -----Generate prediction function-----
+def generate_predition(env_data):
+    """Generate wild fire intensity level based on environmental data
+    """ 
+    # Preprocess   
+    env_data_np = np.array(env_data)
+    env_data_reshape = np.reshape(env_data_np, (-1, 9))
+
+    # Load the model
+    loaded_model = pk.load(open('./Model/model_pickle', 'rb'))
+
+    # Get the prediction
+    result = loaded_model.predict(env_data_reshape)
+
+    print(result)
+
+# generate_predition([94.9, 130.3, 587.1,	14.1, 97.4,	40,	5.8, 0, 1])
